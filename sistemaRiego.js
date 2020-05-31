a4b28f97157f6d0e996c77d81701b7f9753875f5
webbs <salmawebb@hotmail.com>
2020-05-31 14:34:58 -0500

    add background, input, styes

---
+++ sistemaRiego.js
@@ -0,0 +1,19 @@
+var accessToken = "4d0fc494bd7211e80d7f7cf68b68125422667e10";
+var deviceID = "390063001851373237343331"
+var url = "https://api.particle.io/v1/devices/" + deviceID + "/dispensador";
+
+function switchOn()
+{
+	$.post(url, {params: "Open", access_token: accessToken });
+}  
+
+function switchOff()
+{
+	$.post(url, {params: "Close", access_token: accessToken });
+}  
+
+//poner el tiempo
+function switchTimer()
+{
+	}
+	
